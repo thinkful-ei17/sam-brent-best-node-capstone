508b2da74273a58e7990606e6c3eef96831d55d8
Sam Gould <sjg326@gmail.com>
2018-03-19 16:05:33 -0700

    check to see if restaurant is on the wishlist of a user before adding it and alter button text depending on action taken

--- public/js/app.js
+++ public/js/app.js
@@ -142,10 +142,24 @@ function startApp() {
 
     $('#whole-map').on('click', '.js-wishlist-button', function (event) {
       event.preventDefault();
-      api.createWishlistEntry(store.currentUser, store.place)
-        .then(response => {
-          store.data = response;
-          render.render(store);
+      api.searchOne(store.currentUser)
+        .then(user => {
+          for (let i=0; i<user.wishlist.length; i++) {
+            if (user.wishlist[i].restaurant_id.placeId === store.place.place_id) {
+              store.alreadyAdded = true;
+              return $(this).text('Already on Wishlist');
+            } else {
+              $(this).text('ADDED!');
+            }
+          } if (!store.alreadyAdded) {
+            return api.createWishlistEntry(store.currentUser, store.place);
+          }
+        }).then(response => {
+          store.alreadyAdded = false;
+          if (response._id) {
+            store.data = response;
+            render.render(store);
+          }
         }).catch(err => {
           console.error(err);
         });
